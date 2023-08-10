7bf9657986f9dfc87e4c782f360eed595072222a
uzairjacobs <93554483+uzairjacobs@users.noreply.github.com>
2023-08-10 11:34:04 +0200

    changed layout of code

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ FREE_WARNING = "Free shipping only applies to single customer orders";
 BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
 NONE_SELECTED = 0;
 
-const customers = 1;
+const customers = 2;
 const country = "RSA";
 let currency;
 let shipping;
 
-let shoes = 300 * 2;
+let shoes = 300 * 1;
 let batteries = 35 * 2;
 let toys = 100 * 5;
 let pens = 5 * NONE_SELECTED;
@@ -16,19 +16,18 @@ let shirts = 150 * NONE_SELECTED;
 if (country === "RSA") {
   shipping = 400;
   currency = "R";
-  if ((shoes + batteries + pens + shirts + toys) >= 1000 && customers == 1) {
+  if ((shoes + batteries + pens + shirts + toys) >= 1000 && customers === 1) {
+    shipping = 0;
+  } 
+} 
+  
+  else if (country === "NAM") {
+  shipping = 600;
+  currency = "$";
+  if ((shoes + batteries + pens + shirts + toys) >= 1000 && customers === 1) {
     shipping = 0;
   }
 }
-
-else if (country === "NAM") {
-  shipping = 600;
-  currency = "$";
-  if ((shoes + batteries + pens + shirts + toys) >= 1000 && customers == 1) {
-    shipping = 0;
-  }
-  } 
-  
   else if (country === "NK") {
   console.log(BANNED_WARNING);
 
@@ -37,7 +36,7 @@ else if (country === "NAM") {
   currency = "$";
 }
 
-if (customers != 1) {
+if (customers != 1 && shipping == 0) {
   console.log(FREE_WARNING);
 }
 
